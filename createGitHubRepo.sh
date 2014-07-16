76408722f54dc39d6a5ba0b5a2f1c5267492fba9
John Larson <larsonj.home@gmail.com>
2014-07-16 13:57:09 -0400

    added additional comment

--- createGitHubRepo.sh
+++ createGitHubRepo.sh
@@ -2,3 +2,7 @@
 //
 // curl -F 'login=username' -F 'token=API Token' https://github.com/api/v2/yaml/repos/create -F name=reponame
 
+//curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
+//# Remember replace USER with your username and REPO with your repository/application name!
+//git remote add origin git@github.com:USER/REPO.git
+//git push origin master
