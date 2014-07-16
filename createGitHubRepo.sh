14cbbd117eb6f56194dc0493d9937e0e32586c92
John Larson <larsonj.home@gmail.com>
2014-07-16 13:50:00 -0400

    first commit

---
+++ createGitHubRepo.sh
@@ -0,0 +1,4 @@
+// example of gitHub API usage for remote repo creation via commandline.  Requires curl
+//
+// curl -F 'login=username' -F 'token=API Token' https://github.com/api/v2/yaml/repos/create -F name=reponame
+
